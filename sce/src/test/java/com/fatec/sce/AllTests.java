862f1a4460db415144bd5f271660fef2042446bd
professor <professor@L1MP.IPIRANGA.FATEC>
2018-10-25 17:18:15 -0200

    Integração com o banco de dados classe livro

--- sce/src/test/java/com/fatec/sce/AllTests.java
+++ sce/src/test/java/com/fatec/sce/AllTests.java
@@ -5,7 +5,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({TestaDB.class, UC01CadastrarLivro.class, UC05CadastrarUsuario.class })
+@SuiteClasses({
+	TestaDB.class, 
+	UC01CadastrarLivro.class, 
+	UC05CadastrarUsuario.class })
 public class AllTests {
 
 }
